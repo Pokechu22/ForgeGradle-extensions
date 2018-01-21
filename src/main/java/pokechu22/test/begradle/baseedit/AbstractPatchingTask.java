6b48af909e09b0ec8b821ebf0dea029aa4544134
Pokechu22 <Pokechu022@gmail.com>
2018-01-20 16:40:26 -0800

    Also don't print the stacktrace for patching exceptions

--- src/main/java/pokechu22/test/begradle/baseedit/AbstractPatchingTask.java
+++ src/main/java/pokechu22/test/begradle/baseedit/AbstractPatchingTask.java
@@ -223,7 +223,8 @@ public abstract class AbstractPatchingTask extends DefaultTask {
 			try {
 				newContent = DiffUtils.patch(origContent, patch);
 			} catch (PatchFailedException e) {
-				getLogger().warn("Pating failed for " + className + "!", e);
+				getLogger().warn("Pating failed for " + className + "!  " + e);
+				getLogger().info("Exception: " + e);
 				getLogger().warn("Saving unpatched version instead...");
 				newContent = origContent;
 			}
